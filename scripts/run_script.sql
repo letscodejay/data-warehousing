d3f3d3ad7611565f2a008ddd34ceef8f86ed0056
Jayvardhan Sakwar <vardhanjay.sakwar@gmail.com>
2025-09-07 16:57:29 +0530

    final update

--- scripts/run_script.sql
+++ scripts/run_script.sql
@@ -1,3 +1,6 @@
 USE DataWarehouse;
-EXEC bronze.load_bronze
-EXEC silver.load_silver
+EXEC bronze.load_bronze;
+EXEC silver.load_silver;
+SELECT * FROM gold.dim_customers;
+SELECT * FROM gold.dim_products;
+SELECT * FROM gold.fact_sales;
